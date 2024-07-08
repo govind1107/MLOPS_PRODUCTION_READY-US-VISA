b11447f0b40de95c91120ee1b8a7181f57842f80
Govind <govind.lipne11@gmail.com>
2024-07-08 09:59:10 +0530

    added trail notebook

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Govind Lipne",
+    author_email="govind.lipne11@gmail.com",
+    packages=find_packages()
+)
